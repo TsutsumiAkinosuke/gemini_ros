d1d614e340a58a509eb8982863975cc5d809b114
AkinosukeTsutsumi <80503262+TsutsumiAkinosuke@users.noreply.github.com>
2024-10-30 00:56:41 +0900

    Initial commit

---
+++ gemini_ros/gemini_ros.py
@@ -0,0 +1,44 @@
+import rclpy
+from rclpy.node import Node
+from std_msgs.msg import String
+
+import os
+import google.generativeai as genai
+
+class GeminiNode(Node):
+
+    def __init__(self):
+        
+        super().__init__("gemini_node")
+
+        genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
+
+        self.model = genai.GenerativeModel("gemini-pro")
+        self.chat = self.model.start_chat(history=[])
+
+        self.output_msg = String()
+
+        self.text_publisher = self.create_publisher(String, "/gemini/output_text", 10)
+        self.text_subscriber = self.create_subscription(String, "/gemini/input_text", self.gemini_callback, 10)
+    
+    def gemini_callback(self, msg):
+
+        self.get_logger().info(f"Subscribed /input_text: {msg.data}")
+        self.prompt = msg.data
+
+        self.res = self.chat.send_message(self.prompt)
+
+        self.output_msg.data = self.res.text
+        self.text_publisher.publish(self.output_msg)
+
+        self.get_logger().info(f"Published /output_text: {self.output_msg.data}")
+
+def main(args=None):
+
+    rclpy.init(args=args)
+    node = GeminiNode()
+    rclpy.spin(node)
+    rclpy.shutdown()
+
+if __name__ == "__main__":
+    main()
